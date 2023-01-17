df1e51ac4ce4526e55da3f62c26db0ac8bf3b678
jacksonngabonziza <ngabojck@gmail.com>
2023-01-17 23:20:46 +0200

    bg-fx

--- src/main/java/com/cse/api/controller/SensorController.java
+++ src/main/java/com/cse/api/controller/SensorController.java
@@ -3,6 +3,8 @@ package com.cse.api.controller;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 import javax.validation.Valid;
 import javax.xml.transform.Templates;
@@ -11,7 +13,9 @@ import com.cse.api.Response.CommonResponse;
 import com.cse.api.exception.ResourceNotFoundException;
 import com.cse.api.model.Rain;
 import com.cse.api.model.Sensor;
+import com.cse.api.model.harvest;
 import com.cse.api.repository.SensorRepository;
+import com.cse.api.repository.harvestRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -25,20 +29,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.cse.api.repository.RainRepository;
+
 @RestController
 @CrossOrigin
 @RequestMapping("/api/v1")
 public class SensorController {
   @Autowired
   RainRepository rainRepository;
-  
+
+  @Autowired
+harvestRepository harvestRepository;
   @Autowired
   private SensorRepository sensorRepository;
   
   @CrossOrigin(origins = "*")
   @PostMapping("/send")
-  public Sensor createdata(@Valid @RequestBody Sensor sensor) throws ResourceNotFoundException {
-    return sensorRepository.save(sensor);
+  public List<Sensor> createdata(@Valid @RequestBody List<Sensor> sensor) throws ResourceNotFoundException {
+    return sensorRepository.saveAll(sensor);
   }
   @CrossOrigin(origins = "*")
   @GetMapping("/retrieve")
@@ -73,8 +80,33 @@ public class SensorController {
         
         return sensorRepository.findTopByOrderByIdDesc();
       }
+
+
+      @CrossOrigin(origins = "*")
+      @GetMapping("/allraindata")
+      public List<Rain> getAllRainData() {
+        
+        return rainRepository.findAll();
+      }
+
+
+      @CrossOrigin(origins = "*")
+      @GetMapping("/allharvestdata")
+      public List<harvest> getAllHarvestData() {
+        return harvestRepository.findAll();
+      }
       
       
+
+      @CrossOrigin(origins = "*")
+      @GetMapping("/weathermodel")
+      public Sensor getmetadata() {
+        Random random = new Random();
+        int[]arr={};
+        IntStream temp=random.ints(23, 27);
+        System.out.println("£££££££££££££££ "+temp);
+        return sensorRepository.findTopByOrderByIdDesc();
+      }
     
 
 
